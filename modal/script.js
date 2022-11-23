0d52923a703d8ceabcdf7487b50f9626eb9dbc46
Iman Chamileva <iman.moussaevna@gmail.com>
2022-11-23 22:49:00 +0100

    modal

--- modal/script.js
+++ modal/script.js
@@ -1,6 +1,37 @@
 const buttonOpen = document.querySelector('.button-open');
 
-button.addEventListener('click', function () {
+const showModal = document.querySelector('.modalSection2')
 
-    section.classList.buttonOpen
+const modalSection1 = document.querySelector('.modalSection1')
+
+const modalButton = document.querySelector('.buttonClose')
+
+
+buttonOpen.addEventListener('click', function () {
+
+    showModal.style.display = "block";
+
+
+})
+
+buttonOpen.addEventListener('click', function () {
+
+    modalButton.style.display = "block";
+
+})
+
+buttonOpen.addEventListener('click', function () {
+
+    modalSection1.style.display = "none";
+})
+
+
+modalButton.addEventListener('click', function () {
+
+    modalSection1.style.display = "block";
+})
+
+modalButton.addEventListener('click', function () {
+
+    showModal.style.display = "none";
 })
