57563c7f513f6e1949ee223b5b6370594b007bd8
Iman Chamileva <iman.moussaevna@gmail.com>
2022-12-01 14:45:22 +0100

    first part ok but need toggle

---
+++ faq_page/script.js
@@ -0,0 +1,23 @@
+button1 = document.querySelector('.firstPartButton');
+button2 = document.querySelector('.secondPartButton');
+button3 = document.querySelector('.thirdPartButton');
+part1 = document.querySelector('.part1');
+text1= document.querySelector('.text');
+
+button1.addEventListener('click', function () {
+
+    text1.classList.add('showText');
+
+})
+
+button2.addEventListener('click', function () {
+
+    text1.classList.add('showText');
+
+})
+
+button3.addEventListener('click', function () {
+
+    text1.classList.add('showText');
+
+})
