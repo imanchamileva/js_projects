ca835a76e3e4b68adfc05dcd31e32fd684e5ce9d
Iman Chamileva <iman.moussaevna@gmail.com>
2022-12-01 21:28:23 +0100

    project done

--- faq_page/script.js
+++ faq_page/script.js
@@ -2,22 +2,26 @@ button1 = document.querySelector('.firstPartButton');
 button2 = document.querySelector('.secondPartButton');
 button3 = document.querySelector('.thirdPartButton');
 part1 = document.querySelector('.part1');
-text1= document.querySelector('.text');
+text1= document.querySelector('.text1');
+text2= document.querySelector('.text2');
+text3= document.querySelector('.text3');
 
 button1.addEventListener('click', function () {
 
-    text1.classList.add('showText');
+    text1.classList.toggle('showText');
 
 })
 
+
+
 button2.addEventListener('click', function () {
 
-    text1.classList.add('showText');
+    text2.classList.toggle('showText');
 
 })
 
 button3.addEventListener('click', function () {
 
-    text1.classList.add('showText');
+    text3.classList.toggle('showText');
 
 })
